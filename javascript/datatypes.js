6cceb7eb17411db1b7b5d80a5fe104141baba5cb
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-05-31 16:08:22 +0545

    feat : datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,27 @@
+// string
+
+let name = "bunny";
+console.log(name);
+console.log(name[0]);
+
+// boolean
+// true
+// false
+let a = true;
+console.log(a);
+
+let b = null;
+console.log(b);
+
+// array. => collection of similar datatypes
+
+let num = [1, 2, 3, 4, 5, 6, 7];
+console.log(num);
+
+// object 
+let detail = {
+  name: "sarthak",
+  age: 7,
+};
+
+console.log(detail);
