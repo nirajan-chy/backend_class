569e797c4cd8430214733f12f7e3ce19cdf1fb92
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-05-31 15:38:29 +0545

    feat : variable

---
+++ javascript/variable.js
@@ -0,0 +1,14 @@
+// variable
+// let is used for certain scope.
+{
+  let a = 99;
+  console.log(a);
+}
+
+// var is used globally
+var num1 = 66;
+
+// const never change
+
+const PI = 3.14;
+// the value of PI is 3.14 throughout the page
