62656679a65e216877e5bcdc7179aa2481d0fae5
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-06-01 18:57:33 +0545

    feat :

--- javascript/function.js
+++ javascript/function.js
@@ -2,7 +2,14 @@
 let a = 8;
 let b = 7;
 function add() {
-  return  a + b;
+  return a + b;
 }
 let result = add();
 console.log(result);
+
+function sub() {
+  return a - b;
+}
+
+console.log(sub());
+
