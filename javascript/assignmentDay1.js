4ddf90e024dea9b67383b8c485c91f757cf1b621
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-06-01 19:22:30 +0545

    javascript

--- javascript/assignmentDay1.js
+++ javascript/assignmentDay1.js
@@ -1,14 +1,9 @@
 // Print the multiple table of 5 ;
 
-// 5*1=5
-// 5*2=10
-// .
-// .
-// .
-// .a               
-// .
-// 5*10=50
+let num = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${num} * ${i} = ${num * i}`);
+}
 
-
-//day2  assignment 
+//day2  assignment
 // Using a arrow function print prime number upto 79
