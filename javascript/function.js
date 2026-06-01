94cb7d0296d5f3888b9db12d32073d11cab50fdf
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-06-01 19:14:54 +0545

    javascript

--- javascript/function.js
+++ javascript/function.js
@@ -13,7 +13,32 @@ function sub() {
 
 console.log(sub());
 
-function mul( m= 9 , n= 8) {
+function mul(m = 9, n = 8) {
   return m * n;
 }
 console.log(mul());
+
+function sum(m, n) {
+  return x + y;
+}
+console.log(sum((x = 3), (y = 6)));
+
+// arrow function
+const test = () => {
+  return p + q;
+};
+console.log(test((p = 9), (q = 9)));
+
+// promises
+
+// resolve and reject
+// try and catch
+
+const promise = async() => {
+  try {
+    const user = await user.create();
+    console.log("la thik xa ")
+  } catch (error) {
+    console.log("error aayo")
+  }
+};
