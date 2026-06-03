fb4b609f0c50b7bff3ca6044c17d6ac6dec1a9fa
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-06-03 16:11:33 +0545

    feat : database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { PORT } = require("./src/config/env");
+const { connectMongoDB } = require("./src/config/mongoDB");
 
 const app = express();
 
 app.use(express.json());
+connectMongoDB
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ backend/src/config/mongoDB.js
@@ -0,0 +1,14 @@
+const { MONGO_URI } = require("./env");
+const mongoose = require("mongoose")
+const connectMongoDB = ()=>{
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Database connected")
+  } catch (error) {
+    throw new Error(error.meesage);
+  }
+}
+
+module.exports = {
+  connectMongoDB
+}
