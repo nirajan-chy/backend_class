35452dfaf1a4d971c97b20b6e683f6c692456396
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-06-13 22:59:14 +0545

    feat : deploy

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
   },
   {
     timestamps: true,
